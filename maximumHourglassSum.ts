63290b34b4a8c0534a5e1a17b3ac53597a9b85a3
Muhammad Salman <44293345+salmanghazi@users.noreply.github.com>
2021-08-03 15:03:43 +0500

    Calculate maximum hourglass sum in a 6x6 array (ts/js)

--- maximumHourglassSum.ts
+++ maximumHourglassSum.ts
@@ -1,13 +1,10 @@
-function hourglassSum(arr: number[][]): number {
-  // Write your code here
+ function hourglassSum(arr: number[][]): number {
   const [row, col] = [arr.length, arr[0].length];
-  console.log(row, col)
   var i, j;
-  var currentSum = 0,
-    highestSum = 0;
+  var currentSum=-Infinity,
+      highestSum=-Infinity;
   for (i = 0; i < row - 2; i++) {
     for (j = 0; j < col - 2; j++) {
-      //     currentSum+=arr[i][j]
       currentSum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]) + arr[i + 1][j + 1] + (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
       if (currentSum > highestSum) {
         highestSum = currentSum
@@ -15,4 +12,4 @@ function hourglassSum(arr: number[][]): number {
     }
   }
   return highestSum;
-}
+ }
